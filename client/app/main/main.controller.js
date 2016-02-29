caa29c6348bd4dd2e6a51a8ae8b8db5603f1e583
Christopher Mehmed <chrismehmed@gmail.com>
2016-02-29 18:42:15 -0500

    grammer edit

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,13 +5,13 @@ angular.module('portfolioAppApp')
 .value('PROJECTS', [
     {
       name: 'Project Monologues',
-      description: 'An actors\' resource to search, add, and save monologues.  Anyone can browse, with some features reserved for users.  Curators check recently added materials for accuracy and consistency.',
+      description: 'A resource for actors to search, add, and save monologues.  Anyone can browse, with some features reserved for users.  Curators check recently added materials for accuracy and consistency.',
       tools: ['AngularJS', 'NodeJS', 'ExpressJS', 'Mongoose', 'MongoDB', 'uiRouter', 'uiBootstrap', 'grunt', 'jasmine'],
       url: 'http://protected-escarpment-86335.herokuapp.com',
       repo: 'https://github.com/ChildeRowland/ProjectMonologueFullstack.git'
     }, {
       name: 'Simon Workshop',
-      description: 'Exercise replicating the Milton Bradly game Simon',
+      description: 'Exercise replicating the Milton Bradly game Simon.',
       tools: ['AngularJS', 'Bootstrap'],
       url: 'http://mysterious-gorge-35073.herokuapp.com',
       repo: 'https://github.com/ChildeRowland/simonApp.git'
