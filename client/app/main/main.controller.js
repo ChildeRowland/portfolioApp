07f4b310f9c4efc1d3594bcb1412eca385598695
Christopher Mehmed <chrismehmed@gmail.com>
2016-03-07 12:10:11 -0500

    more information in the header

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,21 +5,21 @@ angular.module('portfolioAppApp')
 .value('PROJECTS', [
     {
       name: 'Project Monologues',
-      description: 'A resource for actors to search, add, and save monologues.  Anyone can browse, with some features reserved for users.  Curators check recently added materials for accuracy and consistency.',
+      description: 'A resource for actors to search and study monologues.  Anyone can browse, with some features reserved for users.  Curators check recently added materials for accuracy and consistency.',
       tools: ['AngularJS', 'NodeJS', 'ExpressJS', 'Mongoose', 'MongoDB', 'uiRouter', 'uiBootstrap', 'grunt', 'jasmine'],
-      url: 'http://cm-project-monologue.herokuapp.com',
+      url: 'https://cm-project-monologue.herokuapp.com',
       repo: 'https://github.com/ChildeRowland/ProjectMonologueFullstack.git'
     }, {
       name: 'Simon Workshop',
       description: 'Exercise replicating the Milton Bradly game Simon.',
       tools: ['AngularJS', 'Bootstrap'],
-      url: 'http://cm-simon-exercise.herokuapp.com',
+      url: 'https://cm-simon-exercise.herokuapp.com',
       repo: 'https://github.com/ChildeRowland/simonApp.git'
     }, {
       name: 'Weather Getter',
-      description: 'Single page app, using the Open Weather Api to get temperature by city.',
-      tools: ['AngularJS', 'ngRoute', 'ngResource'],
-      repo: 'https://github.com/ChildeRowland/weatherSPA.git'
+      description: 'Single page app, using the Open Weather Api to get temperature by city.  Also refactored with a commed line interface.  User can get weather information by specifying city, or allow ipinfo.io to set location via IP address.',
+      tools: ['AngularJS', 'ngRoute', 'ngResource', 'yargs', 'OpenWeatherApi' ],
+      multiRepos: ['https://github.com/ChildeRowland/weatherSPA.git', 'https://github.com/ChildeRowland/command-line-weather.git']
     }
   ])
 
@@ -27,7 +27,7 @@ angular.module('portfolioAppApp')
   var self = this;
 
   self.linkedin = 'https://www.linkedin.com/in/christopher-mehmed-8a161545';
-  self.github = 'https://github.com/ChildeRowland';
+  self.github = 'https://github.com/ChildeRowland?tab=repositories';
 
   self.projects = PROJECTS;
 
